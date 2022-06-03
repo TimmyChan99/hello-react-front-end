eb72ddc91b3215fc69109947cf00edeaa51314d3
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-03 18:35:29 +0100

    Create Greeting components and setup redux to fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Greeting from'./components/Greeting';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import store from './redux/configureStore';
 
 function App() {
   return (
-    <div className="App">
-      greeting hi
-    </div>
+      <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path='/' element={ 'Hello'} />
+        <Route path='/greeting' element={<Greeting />} />
+      </Routes>
+    </Router>
+    </Provider>
   );
 }
 
---
+++ src/redux/greeting/greeting.js
@@ -0,0 +1,30 @@
+const GET_MESSAGES = 'GET_MESSAGES';
+
+
+const initialState = {
+	greeting: ''
+}
+
+const getMessagesAction = (payload) => ({
+	type: GET_MESSAGES,
+	payload,
+})
+
+export const getMessagesAPI = () => async (dispatch) => {
+	const getData = await fetch('http://localhost:3001/api/v1/greetings')
+	const resp = await getData.json()
+	dispatch(getMessagesAction(resp.messages))
+}
+
+const greetingReducer = (state = initialState, action) => {
+	switch (action.type) {
+
+	case GET_MESSAGES:
+		return { ...state, greeting: action.payload }
+
+	default:
+		return state
+	}
+}
+
+export default greetingReducer;
